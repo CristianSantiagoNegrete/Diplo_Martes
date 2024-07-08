77a4d2c617d70fb793e3379015159f5cb7a46f37
Santiago Negrete <csantiagonegrete@hotmail.com.ar>
2024-07-08 12:21:19 -0300

    se agregaron los ruteos "prueba", "destacados", "nosotros" y "contacto"

--- M4U3/app.js
+++ M4U3/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactoRouter = require ('./routes/contacto')
 
 var app = express();
 
@@ -21,6 +22,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contacto', contactoRouter);
+
+app.get('/prueba', function(req,res,next){
+  res.send('Hola soy la pagina de prueba')
+})
+
+app.get('/nosotros', function(req,res,next){
+  res.send('Hola soy la pagina de nosotros')
+})
+
+app.get('/destacados', function(req,res,next){
+  res.send('Hola soy la pagina de destacados')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
